9cdfb2da273d39df9c39e073c66a6ee56963148c
Eddie Lee <eddielee@email.com>
2015-06-07 17:17:16 -0700

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		jshint: {
+			all: ['Gruntfile.js', 'app/**/*.js'],
+			options: {
+				node: true,
+				curly: true,
+				eqeqeq: true, 
+				browser: true,
+				globals: {
+					jQuery: true,
+					module: true,
+					require: true, 
+					console: true
+				}
+			}
+
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.registerTask('default', ['jshint']);
+};
